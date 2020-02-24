e51b95d383cf82663fb45094d3acd3dc2c7954fb
rsjaffe <rsjaffe@gmail.com>
2020-02-24 13:10:21 -0800

    Update transform.bat

--- transform.bat
+++ transform.bat
@@ -1,1 +1,2 @@
 "C:\Program Files\Saxonica\SaxonHE9.9N\bin\Transform.exe" -s:master_ioc-names_xml.xml -xsl:birdsbyfamily.xsl -o:birdsfam.txt
+"C:\Program Files\Saxonica\SaxonHE9.9N\bin\Transform.exe" -s:master_ioc-names_xml.xml -xsl:birdsbyorder.xsl -o:birdsord.txt
